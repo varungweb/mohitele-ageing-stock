ba032fc0133f7c6410a9b17e5fffef158620510a
VARUN GUPTA <61659730+varungweb@users.noreply.github.com>
2025-09-07 19:30:22 +0530

    Update lifecycle.sql

--- lifecycle.sql
+++ lifecycle.sql
@@ -3,28 +3,24 @@ WITH ParentGroupCTE AS (
         Code, 
         Name, 
         ParentGrp,
-        -- PGROP logic
         CASE 
             WHEN ParentGrp = 0 THEN Name
-            ELSE 
-            (SELECT TOP 1 NameAlias 
-             FROM Help1 
-             WHERE NameOrAlias = 1 
-               AND Code = ParentGrp)
+            ELSE (SELECT TOP 1 NameAlias 
+                  FROM Help1 
+                  WHERE NameOrAlias = 1 
+                    AND Code = ParentGrp)
         END AS PGROP,
-        -- BGROP logic
         COALESCE(
             (SELECT TOP 1 NameAlias 
              FROM Help1 
              WHERE NameOrAlias = 1 
                AND Code = CM1),
             CASE 
-            WHEN ParentGrp = 0 THEN Name
-            ELSE 
-                (SELECT TOP 1 NameAlias 
-                 FROM Help1 
-                 WHERE NameOrAlias = 1 
-                   AND Code = ParentGrp)
+                WHEN ParentGrp = 0 THEN Name
+                ELSE (SELECT TOP 1 NameAlias 
+                      FROM Help1 
+                      WHERE NameOrAlias = 1 
+                        AND Code = ParentGrp)
             END
         ) AS BGROP
     FROM Master1
@@ -32,34 +28,13 @@ WITH ParentGroupCTE AS (
 ),
 ItemData AS (
     SELECT
-        SerialNo,
-        ROW_NUMBER() OVER (ORDER BY VchType, VchNo) AS SerialNumber,
+        LTRIM(RTRIM(SerialNo)) AS CleanSerialNo,
         (SELECT Name FROM Master1 WHERE Code = ItemCode) AS ITEMNAME, 
         (SELECT Name FROM Master1 WHERE Code = MCCode) AS Series,
         (SELECT Name FROM Master1 WHERE Code = (SELECT ParentGrp FROM Master1 WHERE Code = ItemCode)) AS GROP,
         (SELECT PGROP FROM ParentGroupCTE WHERE Code = (SELECT ParentGrp FROM Master1 WHERE Code = ItemCode)) AS PGROP,
         (SELECT BGROP FROM ParentGroupCTE WHERE Code = (SELECT ParentGrp FROM Master1 WHERE Code = ItemCode)) AS BGROP,
-        VchType,
-        CASE VchType
-            WHEN 1 THEN 'PreviousYears'
-            WHEN 2 THEN 'PurchaseOnly'
-            WHEN 3 THEN 'SalesReturn'
-            WHEN 5 THEN 'StockTransfer'
-            WHEN 8 THEN 'StockJournal'
-            WHEN 9 THEN 'SaleOnly'
-            WHEN 10 THEN 'PurchaseReturn'
-            ELSE CAST(VchType AS VARCHAR)
-        END AS VchTypeName,
-        Value1 AS MainQty, 
-        Value2 AS AltQty, 
-        LTRIM(RTRIM(SerialNo)) As CleanSerialNo,
         Date,
-        VchNo,
-        VchCode,
-        VchItemSN,
-        GridSN,
-        D1,
-        MonthVal,
         CASE VchType
             WHEN 1 THEN 'transfer from PreviousYears stock'
             WHEN 2 THEN 'Purchased this year'
@@ -70,31 +45,31 @@ ItemData AS (
             WHEN 10 THEN 'return to whome, we purchased'
         END AS LifeCyclePhase
     FROM ItemSerialNo
+),
+Numbered AS (
+    SELECT 
+        d.CleanSerialNo,
+        d.ITEMNAME,
+        d.Series,
+        d.GROP,
+        d.PGROP,
+        d.BGROP,
+        d.Date,
+        d.LifeCyclePhase,
+        ROW_NUMBER() OVER (PARTITION BY d.CleanSerialNo ORDER BY d.Date) AS PhaseNo
+    FROM ItemData d
 )
 SELECT 
-    i.SerialNumber,
-    i.ITEMNAME,
-    i.Series,
-    i.GROP,
-    i.PGROP,
-    i.BGROP,
-    i.VchType,
-    i.VchTypeName,
-    i.MainQty,
-    i.AltQty,
-    i.CleanSerialNo AS SerialNo,
-    i.Date,
-    i.VchNo,
-    i.VchCode,
-    i.VchItemSN,
-    i.GridSN,
-    i.D1,
-    i.MonthVal,
-    -- full lifecycle in one column
-    lc.FullLifeCycle
-FROM ItemData i
-OUTER APPLY (
-    SELECT STRING_AGG(LifeCyclePhase, ' -> ') 
-    FROM ItemData d 
-    WHERE d.CleanSerialNo = i.CleanSerialNo
-) lc(FullLifeCycle)
+    n.CleanSerialNo AS SerialNo,
+    MAX(n.ITEMNAME) AS ITEMNAME,
+    MAX(n.Series) AS Series,
+    MAX(n.GROP) AS GROP,
+    MAX(n.PGROP) AS PGROP,
+    MAX(n.BGROP) AS BGROP,
+    STRING_AGG(
+        CAST(n.PhaseNo AS VARCHAR(10)) + '. ' 
+        + n.LifeCyclePhase + ' on ' + CONVERT(VARCHAR(10), n.Date, 120),
+        CHAR(10)
+    ) WITHIN GROUP (ORDER BY n.PhaseNo) AS LifecycleStory
+FROM Numbered n
+GROUP BY n.CleanSerialNo
